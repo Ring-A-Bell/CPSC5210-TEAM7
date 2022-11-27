322e00883e2b4b3e19f61da21fba0bb1f0c88b2d
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-11-27 01:09:35 -0800

    Added unit test for label142_Click method in Form1Tests.cs

--- UnitTestProject1/Form1Tests.cs
+++ UnitTestProject1/Form1Tests.cs
@@ -104,6 +104,19 @@ namespace Pacman_Zagorschi_Franco
         }
 
         [TestMethod]
+        public void label142_Click_privateMethod_null()
+        {
+            // set
+            var form = new Pacman_Zagorschi_Franco.Form1();
+            object sender = null;
+            EventArgs e = null;
+            PrivateObject accessor = new PrivateObject(form);
+
+            // act
+            accessor.Invoke("label142_Click", sender, e);
+        }
+
+        [TestMethod]
         public void button1_MouseLeave_resource_equals_playgame()
         {
             //Set
